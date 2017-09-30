c3b967afa21ab672c027a136d01f747dedecb3d0
Li Yicheng <liyicheng340@gmail.com>
2017-09-30 14:39:02 +0800

    world generator even mroe lolefasfndsjkfnds fdsakfdnas send help am dead inside

--- WorldGenerator/generator.py
+++ WorldGenerator/generator.py
@@ -20,12 +20,15 @@ def rgba2hex(rgba_color):
 	blue = int(rgba_color[2])
 	return '{r:02x}{g:02x}{b:02x}'.format(r = red, g = green, b = blue)
 
+outputBuffer = ""
+
 w, h = img.size
-print(str(w) + "," + str(h))
+outputBuffer += str(w) + "," + str(h) + ","
+
 for y in range(h):
-	line = ""
+	
 	for x in range(w):
-		line += str(blockID[rgba2hex(pix[x, y])])
+		outputBuffer += str(blockID[rgba2hex(pix[x, y])])
 		if x < w-1:
-			line += ","
-	print(line)
+			outputBuffer += ","
+print(outputBuffer)
