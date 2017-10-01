222e2012b3e09affe9c9fbb4bb7455a0f05351f5
Ragul Balaji <ragulbaby@gmail.com>
2017-10-01 10:32:03 +0800

    SPRITES! LABELS! SENSORS! MAGIX! 🏳️‍🌈

--- WorldGenerator/generator.py
+++ WorldGenerator/generator.py
@@ -3,12 +3,15 @@ from PIL import Image
 filename = "world.png"
 
 blockID = {
-    "000000": "",  # Null
+    "000000": "", # Null
     "ffffff": 1,  # Wall
     "ff0000": 2,  # Start Point
     "2c0058": 3,  # Generic Floor
     "0000ff": 4,  # Generic Button
     "ffff00": 5,  # Generic Sensor
+	"ffff44": "'PS'",  # Presense Sensor
+	"ffffaa": "'DHT'",  # DHT Sensor
+	"0000af": "'TV'",  # TV DEVICE
 }
 
 img = Image.open(filename)
